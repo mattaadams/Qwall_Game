c68b441961a90c6f95d4c44f0e2b33fcef75dc5e
Matt Adams <mattaadams1@gmail.com>
2022-03-05 17:55:01 -0500

    game level platforms

---
+++ game_level.py
@@ -0,0 +1,30 @@
+import pygame
+from settings import Settings
+
+
+class Level():
+    def __init__(self, data):
+        self.tile_list = []
+        self.settings = Settings()
+        # load images
+        # dirt_img = pygame.image.load('img/dirt.png')
+        # grass_img = pygame.image.load('img/grass.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    x = col_count * self.settings.tile_size
+                    y = row_count * self.settings.tile_size
+                    color = (0, 0, 0)
+                    tile = (color, pygame.Rect((x, y), (self.settings.tile_size, self.settings.tile_size)))
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    pass
+                col_count += 1
+            row_count += 1
+
+    def draw(self, screen):
+        for tile in self.tile_list:
+            pygame.draw.rect(screen, tile[0], tile[1])
